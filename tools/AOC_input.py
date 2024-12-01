47cacc66b26713e70f0eac77644b8ad662c064e2
peter <peter.m.schrader@gmail.com>
2024-12-01 16:58:38 +0100

    corrected type(data)

--- tools/AOC_input.py
+++ tools/AOC_input.py
@@ -10,16 +10,14 @@ def get_input(day: int, year: int = YEAR) -> list:
 	file_name = f"data/day_{day:0>2}.txt"
 
 	if os.path.exists(file_name):
-		with open(file_name) as f:
-			data: list = f.read().split("\n")
+		with open(file_name) as f: data: list = f.readlines()
 		return data
 
-	data: list = get_data(session=SESSIONS[YEAR], day=day, year=year, block=False).split("\n")
+	data: str = get_data(session=SESSIONS[YEAR], day=day, year=year, block=False)
 	if data:
-		with open(file_name, "w") as f:
-			f.writelines(data)
+		with open(file_name, "w") as f:	f.write(data)
 		print(f"puzzle input written to {file_name}")
-	return data
+	return data.split("\n")
 
 
 if __name__ == '__main__':
