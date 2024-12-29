cd989680a0edb2329bce2a5bd9d59bbc8a495bd4
peter <peter.m.schrader@gmail.com>
2024-12-29 12:46:35 +0100

    done day 12.2 and 13.2

--- day_12.py
+++ day_12.py
@@ -1,9 +1,6 @@
-# copy data to test.txt and in.txt
-# aliases in terminal: 'aot' for test, 'aos' for puzzle input, 'aoc' to run both
 from collections import defaultdict
 
 DIRECTIONS4 = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-DIRECTIONS8 = ((dr, dc) for dr in (-1, 0, 1) for dc in (-1, 0, 1) if (dr, dc) != (0, 0))
 
 
 def on_grid(r, c, rows, cols):
@@ -12,43 +9,25 @@ def on_grid(r, c, rows, cols):
 
 def floodfill(a: set, r: int, c: int, t: str) -> set:
     for dr, dc in DIRECTIONS4:
-        if not on_grid(r + dr, c + dc, rows, cols): continue
-        if grid[r + dr][c + dc] != t: continue
-        if (r + dr, c + dc) in a: continue
+        if not on_grid(r + dr, c + dc, rows, cols) or grid[r + dr][c + dc] != t or (r + dr, c + dc) in a:
+            continue
         a |= floodfill(a | {(r + dr, c + dc)}, r + dr, c + dc, t)
     return a
 
 
+def border_map(r, c) -> list[bool]:  # u, r, d, l: true when there is a border
+    return [not on_grid(r + dr, c + dc, rows, cols) or grid[r + dr][c + dc] != grid[r][c] for dr, dc in DIRECTIONS4]
+
+
 part1 = part2 = 0
-# grid = open(0).read().splitlines()
+
 # with open("test.txt", "r") as f: grid = f.read().splitlines()
 with open("in.txt", "r") as f: grid = f.read().splitlines()
 cols, rows = len(grid[0]), len(grid)
 
 regions = defaultdict(list)  # need calculate to separate regions of same type
 seen = set()
-borders = defaultdict(list)
-corners = defaultdict(int)
-
-
-def border_map(r, c) -> list[bool]:
-    # u, r, d, l: true when there is a border
-    return [not on_grid(r+dr, c+dc, rows, cols) or grid[r+dr][c+dc] != grid[r][c] for dr, dc in DIRECTIONS4]
-
-
-def is_enclave(t2: str, j: int, inner: set, outer: set) -> bool:
-
-    # number of pos of inner that have border to outer == outer_edges[inner]
-    n = 0
-    for r, c in inner:
-        u, right, d, l = borders[(r,c)]
-        if u: n += (r-1, c) in outer
-        if right: n += (r, c+1) in outer
-        if d: n += (r+1, c) in outer
-        if l: n += (r, c-1) in outer
-
-    return perimeters[t2][j] == n
-
+borders = dict()
 
 for r, row in enumerate(grid):
     for c, t in enumerate(row):
@@ -56,74 +35,32 @@ for r, row in enumerate(grid):
             flooded: set = floodfill({(r, c)}, r, c, t)
             seen |= flooded
             regions[t].append(flooded)
-        borders[(r, c)] = border_map(r, c)
-perimeters = defaultdict(list)
-for t, regs in regions.items():
-    t_sum = 0
-    for reg in regs:
-        perimeter = 0
-        for pos in reg:
-            perimeter += sum(borders[pos])
-        perimeters[t].append(perimeter)
-        t_sum += perimeter * len(reg)
-    part1 += t_sum
-print(part1)        # 1485656
-print(perimeters)
-# 1. follow outer perimeter of each region keep count of turns to calculate straight edges
-# 2. check if any "islands" inside a region, and add straight edges of that outer perimeter
+        borders[r, c] = border_map(r, c)
 
-outer_edges = defaultdict(list)
 for t, regs in regions.items():
     for reg in regs:
-        start, edges = min(reg), 0
-        r, c = start
-        dr, dc = 0, 1   # initial walking direction to right
-        b, no_b = 0, 1   # initial position has border at top, no right border
-        assert borders[start][3]    # inital position has left border
-        while not (edges >= 4 and (r, c) == start and (dr, dc) == (0, 1)):
-            while borders[(r, c)][b] and not borders[(r, c)][no_b]:
-                r, c = r+dr, c+dc
+        perimeter = sum(sum(borders[pos]) for pos in reg)
+        part1 += perimeter * len(reg)
 
-            if borders[(r, c)][b] and borders[(r, c)][no_b]:  # -> turn CW
-                dr, dc = dc, -dr
-                edges += 1
-                b += 1
-                if b == 4: b = 0
-                no_b += 1
-                if no_b == 4: no_b = 0
-                continue
+print(f"{part1=}")  # 1485656
 
-            if not borders[(r, c)][b]:  # -> turn CCW
-                dr, dc = -dc, dr
-                r, c = r+dr, c+dc
-                edges += 1
-                b -= 1
-                if b == -1: b = 3
-                no_b -= 1
-                if no_b == -1: no_b = 3
-                continue
-
-        outer_edges[t].append(edges)
-
-print(outer_edges)
-
-enclaves = defaultdict(list)
+# count all corners of a region (= number of straight lines)
+corner_map = defaultdict(set)
 for t, regs in regions.items():
-    for i, reg in enumerate(regs):
-        for t2, regs2 in regions.items():
-            if t == t2: continue
-            for i2, reg2 in enumerate(regs2):
-                if is_enclave(t2, i2, reg2, reg):
-                    enclaves[t].append((i, t2, i2))
-print(enclaves)
+    for reg in regs:
+        for r, c in reg:
+            corner_pos = {(r - 0.5, c - 0.5), (r - 0.5, c + 0.5), (r + 0.5, c - 0.5), (r + 0.5, c + 0.5)}
+            corner_map[t] |= corner_pos
 
 for t, regs in regions.items():
-    for i, reg in enumerate(regs):
-        edges = outer_edges[t][i]
-        for i2, t2, j in enclaves[t]:
-            if i2 != i: continue
-            edges += outer_edges[t2][j]
-        print(t, edges)
-        part2 += len(reg) * edges
+    for reg in regs:
+        corners = 0
+        for r5, c5 in corner_map[t]:
+            corner_type = []
+            for r, c in ((r5 - .5, c5 - .5), (r5 - .5, c5 + .5), (r5 + .5, c5 + .5), (r5 + .5, c5 - .5)):
+                corner_type.append((r, c) in reg)   # top-left, top-right, bottom-right, bottom-left
+            corners += sum(corner_type) in (1, 3)   # 1 outer or 1 inner corner
+            corners += 2 * (corner_type in [[False, True, False, True], [True, False, True, False]])  # 2 outer corners
+        part2 += corners * len(reg)
 
-print(part2)    # 911384 too high
+print(f"{part2=}")  # 899196
--- day_13.py
+++ day_13.py
@@ -1,91 +1,39 @@
-# copy data to test.txt and in.txt
-# aliases in terminal: 'aot' for test, 'aos' for puzzle input, 'aoc' to run both
 import re
-import math
+
+
+def solve_2_unknowns(xa, ya, xb, yb, x0, y0):
+    tol = 2
+    A = round((y0 - (x0 * yb / xb)) / (ya - (xa * yb / xb)), tol)
+    B = round((x0 - A * xa) / xb, tol)
+    return A, B
+
+
+# filename = "test.txt"
+filename = "in.txt"
+with open(filename, "r") as f: data = f.read()
+
 part1 = part2 = 0
-# grid = open(0).read().splitlines()
-# with open("test.txt", "r") as f: data = f.read()
-with open("in.txt", "r") as f: data = f.read()
-machines = [list(map(int, re.findall("\d+", s))) for s in data.split("\n\n")]
-presses = []
-# for ax, ay, bx, by, px, py in machines:
-#     found = False
-#     na = 0
-#     while not found:
-#         if na > 100: break
-#         nb = 0
-#         while not found:
-#             if nb > 100: break
-#             if na * ax + nb * bx == px and na * ay + nb * by == py:
-#                 presses.append((na, nb))
-#                 found = True
-#             nb += 1
-#             if nb > 100 or na > 100: exceeded = True
-#         na += 1
-#     # print(presses)
-# part1 = sum(na * 3 + nb for na, nb in presses)
-#
-# print(part1)
 
+machines = [list(map(int, re.findall(r"\d+", s))) for s in data.split("\n\n")]
 
-# Alternative
-print()
-print("Alternative")
-presses = []
-for ax, ay, bx, by, px, py in machines:
-    # X: if ax>3*bx : cost to move x by A is cheaper
-    # Y: if ay>3*by : cost to move y by A is cheaper
-    first_direction = (ax, bx)
-    if ax<3*bx:
-        first_direction = (bx, ax)
+for m in machines:
+    A, B = solve_2_unknowns(*m)
+    if A % 1 or B % 1: continue
+    if not (0 <= A <= 100 and 0 <= B <= 100): continue
+    part1 += int(A) * 3 + int(B)
 
-    else:
+print(f"{part1= }")
 
+delta = 10000000000000
 
+for i in range(len(machines)):
+    machines[i][4] += delta
+    machines[i][5] += delta
 
+for m in machines:
+    A, B = solve_2_unknowns(*m)
+    if A % 1 or B % 1: continue
+    part2 += int(A) * 3 + int(B)
 
-    # >1 favour A, <1 favour B
-    # x_ratio = ax / (3 * bx)
-    # y_ratio = ay / (3 * by)
+print(f"{part2= }")  # 104140871044942
 
-    # px += 10000000000000
-    # py += 10000000000000
-
-    lcm_x = math.lcm(ax, bx)
-    lcm_y = math.lcm(ay, by)
-    # print(lcm_x, lcm_y)
-
-    # find solution for x with max A:
-    na = px // ax  # max steps of ax to px,
-    nb = 0
-    na_r = px % ax  # 0 <= na_r < ax
-    if na_r == 0:  # for x only A needed
-        pass
-
-    if na_r > 0:  # need to reduce na to match any*nb
-        while na_r % bx > 0:
-            na -= 1
-            na_r += ax
-            if na_r > lcm_x: break
-            nb = na_r // bx
-
-    print(f"na: {na}   nb: {nb}")
-    if na * ax + nb * bx == px:  # solution found for x
-        print(f"first solution for x is {na}, {nb}")
-        second = True
-        while na * ay + nb * by != py and second:  # solution also for y => final solution
-            na -= lcm_x // ax
-            if na<0 : second = False
-            print(f"first solution for x is {na}, {nb}")
-            nb += lcm_x // bx
-        if second:
-            print(f"solution is {na}, {nb} ")
-            presses.append((na, nb))
-        else:
-            print("no solution")
-    else:
-        print("no solution for na")
-
-    print()
-part2 = sum(na * 3 + nb for na, nb in presses)
-print(part2)
